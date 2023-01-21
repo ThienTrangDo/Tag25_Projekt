101140e24edc4e0315842f0b1244abd81e267659
Trang Do <trang93@hotmail.de>
2023-01-21 22:20:45 +0100

    Initial commit

---
+++ src/KartenSpielen.kt
@@ -0,0 +1,139 @@
+//Auf gehts eine Runde BlackJack! Hier ist ein vollständiges Kartendeck.
+
+val kartendeck: List<String> = listOf(
+    "Pik Ass", "Pik Koenig", "Pik Dame", "Pik Bube", "Pik 10", "Pik 9",
+    "Pik 8", "Pik 7", "Pik 6", "Pik 5", "Pik 4", "Pik 3", "Pik 2",
+    "Kreuz Ass", "Kreuz Koenig", "Kreuz Dame", "Kreuz Bube", "Kreuz 10", "Kreuz 9",
+    "Kreuz 8", "Kreuz 7", "Kreuz 6", "Kreuz 5", "Kreuz 4", "Kreuz 3", "Kreuz 2",
+    "Herz Ass", "Herz Koenig", "Herz Dame", "Herz Bube", "Herz 10", "Herz 9",
+    "Herz 8", "Herz 7", "Herz 6", "Herz 5", "Herz 4", "Herz 3", "Herz 2",
+    "Karo Ass", "Karo Koenig", "Karo Dame", "Karo Bube", "Karo 10", "Karo 9",
+    "Karo 8", "Karo 7", "Karo 6", "Karo 5", "Karo 4", "Karo 3", "Karo 2",
+)
+
+fun main() {
+
+
+/*Die Variable meinDeck ist euer Deck mit dem ihr arbeiten könnt.
+    Mit der Funktion mischen() könnt ihr das Deck mischen.
+    Mit der Funktion eineKarteZiehen() könnt ihr aus eurem Deck die oberste Karte ziehen.
+    Beachtet, dass die Funktion die Karte auch aus eurem Deck entfernt.
+    Mit der Funktion kartenWert() bekommst du den Wert einer Karte als Int zurück.
+    Unter dem Kommentar siehst du wie du die Funktionen benutzen kannst.
+     */
+
+    var meinDeck = kartendeck.toMutableList()
+
+    mischen(meinDeck)
+    var karte = eineKarteZiehen(meinDeck)
+    println(karte)
+    var beispielwert = kartenWert(karte)
+    println(beispielwert)
+
+
+
+
+}
+
+fun mischen(deck: MutableList<String>) {
+    deck.shuffle()
+}
+
+fun eineKarteZiehen(deck: MutableList<String>): String {
+    var karte = deck.first()
+    deck.remove(deck.first())
+    return karte
+}
+
+fun kartenWert(karte: String): Int {
+    var kartenWert = karte.split(" ")[1]
+
+    return when (kartenWert) {
+        "2" -> 2
+        "3" -> 3
+        "4" -> 4
+        "5" -> 5
+        "6" -> 6
+        "7" -> 7
+        "8" -> 8
+        "9" -> 9
+        in listOf("10", "Koenig", "Dame", "Bube") -> 10
+        "Ass" -> 11
+        else -> 0
+    }
+}
+
+/*fun punktzahlHand(hand: MutableList<String>): Int {
+    var sum = 0
+    for (karte in hand) {
+        karte.drop(1)
+    }
+    return sum
+}*/
+fun punktzahlHand(hand: MutableList<String>): Int {
+    var ergebnis = 0
+    for (karte in hand) {
+        ergebnis += kartenWert(karte)
+    }
+    return ergebnis
+}
+
+fun handZiehen(deck: MutableList<String>, name: String): MutableList<String>{
+    var hand: MutableList<String> = mutableListOf()
+
+    // Ziehe zwei Karten und füge sie der Hand hinzu
+    var neueKarte = eineKarteZiehen(deck)
+    hand.add(neueKarte)
+    hand.add(eineKarteZiehen(deck))
+
+    if (name == "Spieler1"){
+        println(hand)
+    }
+    if (name == "Computer"){
+        val ersteKarteWert = kartenWert(hand[0])
+        val zweiteKarteWert = kartenWert(hand[1])
+        val maxWert = maxOf(ersteKarteWert, zweiteKarteWert)
+        println("Der höhere Wert ist: $maxWert")
+    }
+
+    return hand
+}
+
+fun istPunktzahlZuHoch (hand: MutableList<String>): Boolean {
+    return punktzahlHand(hand) > 21
+}
+
+/*
+Nun benötigen wir eine Funktion, die testet, ob eine Hand mehr als 21 Punkte hat und somit verloren hat.
+Als Parameter erwartet die Funktion eine Hand als MutableList, und als Rückgabewert bekommt man einen Boolean.
+Wenn die Hand mehr als 21 Punkte wert ist, ist der Boolean true, ansonsten false.
+ */
+
+fun hitAndStand(deck: MutableList<String>, spieler: MutableList<String>, com: MutableList<String>){
+    // Karte ziehen (Hit) oder lassen (Stand)
+    var eingabe = readln()
+    if (eingabe == "Hit"){
+        var karteGezogen = eineKarteZiehen(deck)
+        spieler.add(karteGezogen)
+    }
+}
+
+
+
+/*
+Als nächstes benötigen wir eine Funktion, die dem Spieler ermöglicht, weitere Karten ziehen zu können.
+Der Spieler wird über die Konsole gefragt, ob er eine weitere Karte ziehen möchte ("hit") oder ob er es lassen will ("stand").
+Die Antwort des Spielers wird über die Konsole mit der readln()-Funktion eingelesen.
+
+Nachdem eine Karte gezogen wurde, soll die aktualisierte Hand und die Punktzahl der Hand über die Konsole ausgegeben werden.
+Ebenfalls muss nach jeder gezogenen Karte zunächst geprüft werden, ob die Hand verloren hat, also ob die Hand mehr als 21 Punkte wert ist.
+
+Schreibt euch also eine geeignete Schleife, die so lange läuft, bis der Spieler verloren hat, oder keine Karten mehr ziehen möchte.
+ */
+
+/*
+Wenn der Spieler keine Karten mehr möchte, zieht der Dealer. Der Dealer ist verpflichtet, neue Karten zu ziehen,
+solange seine Hand weniger als 17 Punkte wert ist. Ist seine Hand mehr als 17 Punkte wert, hört der Dealer auf zu ziehen.
+Schreibt euch dafür wieder eine geeignete Schleife.
+Ihr solltet auch hier nach jeder gezogenen Hand überprüfen, ob der Dealer bereits verloren (Punktezahl der Hand > 21) hat.
+ */
